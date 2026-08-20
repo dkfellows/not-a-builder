2228276c1381f322d14267f02468dd5085426eb2
Donal K. Fellows <donal.k.fellows@manchester.ac.uk>
2026-08-20 12:16:33 +0100

    JEP 513 is very relevant here

--- src/main/java/com/github/dkfellows/notabuilder/ThingRecord.java
+++ src/main/java/com/github/dkfellows/notabuilder/ThingRecord.java
@@ -8,32 +8,8 @@ package com.github.dkfellows.notabuilder;
  * @param quux The quux field.
  */
 public record ThingRecord(int foo, int bar, double grill, String quux) {
-    private static final Foo DEFAULT_FOO = new Foo(0);
-    private static final Bar DEFAULT_BAR = new Bar(0);
-    private static final Grill DEFAULT_GRILL = new Grill(0.0);
-    private static final Quux DEFAULT_QUUX = new Quux("");
-
-    /** Helper because the canonical constructor must be called as the first statement.
-     * @param <T> The type of argument being extracted.
-     * @param args The argument list to extract from.
-     * @param defaultValue The default for the argument being extracted.
-     *      Note that <em>this also tells the runtime code what argument to extract</em>.
-     * @return The extracted argument, if present, or a default.
-     */
-    private static <T extends Args> T select(Args[] args, T defaultValue) {
-        T val = defaultValue;
-        for (var arg: args) {
-            if (arg.getClass() == val.getClass()) {
-                @SuppressWarnings("unchecked")
-                var thisVal = (T) arg;
-                val = thisVal;
-            }
-        }
-        return val;
-    }
-
     /**
-     * Make an instance of the record. 
+     * Make an instance of the record.
      * @param args The labelled non-default arguments to pass.
      * @see ThingRecord.Args#foo(int)
      * @see ThingRecord.Args#bar(int)
@@ -41,10 +17,24 @@ public record ThingRecord(int foo, int bar, double grill, String quux) {
      * @see ThingRecord.Args#quux(String)
      */
     public ThingRecord(Args... args) {
-        this(select(args, DEFAULT_FOO).foo(),
-            select(args, DEFAULT_BAR).bar(),
-            select(args, DEFAULT_GRILL).grill(),
-            select(args, DEFAULT_QUUX).quux());
+        // Defaults
+        var foo = 0;
+        var bar = 0;
+        var grill = 0.0;
+        var quux = "";
+
+        // Extract the args
+        for (var arg: args) {
+            switch (arg) {
+            case Foo f -> foo = f.foo();
+            case Bar b -> bar = b.bar();
+            case Grill g -> grill = g.grill();
+            case Quux q -> quux = q.quux();
+            }
+        }
+
+        // Pass to the primary, auto-generated constructor
+        this(foo, bar, grill, quux);
     }
 
     /** Argument labeller. */
@@ -57,7 +47,7 @@ public record ThingRecord(int foo, int bar, double grill, String quux) {
         public static Args foo(int value) {
             return new Foo(value);
         }
-    
+
         /**
          * Label a value as a {@link ThingRecord#bar()}.
          * @param value The value to label.
@@ -66,7 +56,7 @@ public record ThingRecord(int foo, int bar, double grill, String quux) {
         public static Args bar(int value) {
             return new Bar(value);
         }
-    
+
         /**
          * Label a value as a {@link ThingRecord#grill()}.
          * @param value The value to label.
@@ -75,7 +65,7 @@ public record ThingRecord(int foo, int bar, double grill, String quux) {
         public static Args grill(double value) {
             return new Grill(value);
         }
-    
+
         /**
          * Label a value as a {@link ThingRecord#quux()}.
          * @param value The value to label.
@@ -85,9 +75,9 @@ public record ThingRecord(int foo, int bar, double grill, String quux) {
             return new Quux(value);
         }
     }
-    
+
     private record Foo(int foo) implements Args {}
     private record Bar(int bar) implements Args {}
     private record Grill(double grill) implements Args {}
     private record Quux(String quux) implements Args {}
-}    
+}
